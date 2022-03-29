f15fdf29a65d87bea540c2378e78ed2995c6826e
priyank96 <priyankbhandia@gmail.com>
2022-03-29 03:10:41 -0400

    cahnged to colab paths

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
-PARENT_FOLDER = 'C:\\Users\\priya\\Documents\\IDL\\Project\\'
-DATA_PATH = 'C:\\Users\\priya\\Documents\\IDL\\Project\\data'
-API_COMPANY_DATA_PATH = PARENT_FOLDER +  'data\\api_company_data'
-COMPANY_DATA_PATH = PARENT_FOLDER + 'data\\company_data'
+PARENT_FOLDER = '/content/idl-spring-22-project-deepar'
+DATA_PATH = PARENT_FOLDER + '/data'
+API_COMPANY_DATA_PATH = DATA_PATH +  '/api_company_data'
+COMPANY_DATA_PATH = DATA_PATH + '/company_data'
 
 WINDOW_SIZE = 10
 TRAIN_TEST_SPLIT = 0.7
